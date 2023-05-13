dc4a131264aceedd4c0b99b0223677ebe4be356d
Punit Arani <punitsai36@gmail.com>
2023-05-13 15:21:25 -0700

    Move load_file() and load_files() from app.py to loader.py

--- app.py
+++ app.py
@@ -1,52 +1,16 @@
 """Streamlit App"""
 
-import tempfile
 import threading
 from queue import Queue
 
 import streamlit as st
-from langchain.document_loaders import PyMuPDFLoader
 from langchain.schema import Document
-from langchain.text_splitter import CharacterTextSplitter
-from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 from rabbithole import summarize_document
+from rabbithole.loader import load_files
 
 # Global variables
 q = Queue()
-
-
-def load_file(file: UploadedFile) -> list[Document]:
-    """
-    Load a file and return a list of Document objects
-    :param file: File to load.
-    Supported file types: PDF
-    :return: List of Document objects
-    """
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-
-    if file.type == "application/pdf":
-        # Save to temporary file
-        temp_file = tempfile.NamedTemporaryFile(delete=False)
-        temp_file.write(file.read())
-        temp_file.close()
-        # Load the file using PyMuPDF
-        return PyMuPDFLoader(file_path=temp_file.name).load_and_split(text_splitter=text_splitter)
-    else:
-        raise ValueError(f"Unsupported file type: {file.type}")
-
-
-def load_files(files: list[UploadedFile]) -> dict[str, list[Document]]:
-    """
-    Load a list of files and return a dictionary of Document objects
-    :param files: List of files to load.
-    Supported file types: PDF
-    :return: Dictionary of Document objects
-    """
-    documents = {}
-    for file in files:
-        documents[file.name] = load_file(file)
-    return documents
 
 
 def run_summarization(q: Queue, document: list[Document]):

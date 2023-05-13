4ffeb9da639257e8ec2a411740cca6cd8266e3fd
Punit Arani <punitsai36@gmail.com>
2023-05-13 12:01:12 -0700

    Update app to summarize text

--- app.py
+++ app.py
@@ -2,4 +2,13 @@
 
 import streamlit as st
 
+from rabbithole import summarize_text
+
 st.title("RabbitHole")
+
+input_text = st.text_area("Enter your text (up to 1000 words)", height=200)
+
+if st.button("Summarize"):
+    # TODO: Add loading animation while running
+    st.write("Summarized Text:")
+    st.write(summarize_text(input_text))

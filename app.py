6c68101d09434981e92fc67dfcd50f8f820f0d4e
Punit Arani <punitsai36@gmail.com>
2023-05-13 13:30:55 -0700

    Add load_file()

--- app.py
+++ app.py
@@ -7,11 +7,30 @@ from queue import Queue
 import streamlit as st
 from langchain.document_loaders import PyMuPDFLoader
 from langchain.schema import Document
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 from rabbithole import summarize_document
 
 # Global variables
 q = Queue()
+
+
+def load_file(file: UploadedFile) -> list[Document]:
+    """
+    Load a file and return a list of Document objects
+    :param file: File to load.
+    Supported file types: PDF
+    :return: List of Document objects
+    """
+    if file.type == "application/pdf":
+        # Save to temporary file
+        temp_file = tempfile.NamedTemporaryFile(delete=False)
+        temp_file.write(file.read())
+        temp_file.close()
+        # Load the file using PyMuPDF
+        return PyMuPDFLoader(file_path=temp_file.name).load_and_split()
+    else:
+        raise ValueError(f"Unsupported file type: {file.type}")
 
 
 def run_summarization(q: Queue, document: list[Document]):
@@ -28,13 +47,8 @@ if st.button("Summarize"):
         st.warning("Please upload a file first.")
         st.stop()
 
-    # Save file as temporary file
-    temp_file = tempfile.NamedTemporaryFile(delete=False)
-    temp_file.write(uploaded_file.read())
-    temp_file.close()
-
     # Load the text from the uploaded PDF file
-    text = PyMuPDFLoader(file_path=temp_file.name).load_and_split()
+    text = load_file(uploaded_file)
 
     # Start the summarization in a separate thread
     thread = threading.Thread(target=run_summarization, args=(q, text))

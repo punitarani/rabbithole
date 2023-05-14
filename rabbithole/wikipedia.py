4bdf41eed6614b410e327e821493ea6062de7147
Punit Arani <punitsai36@gmail.com>
2023-05-13 17:30:27 -0700

    Add wikipedia_collection

--- rabbithole/wikipedia.py
+++ rabbithole/wikipedia.py
@@ -2,7 +2,32 @@
 
 from datasets import load_dataset
 
+from vecstore import client
+
 wikipedia_dataset = load_dataset("Cohere/wikipedia-22-12-simple-embeddings", split="train", streaming=False)
+wikipedia_collection = client.create_collection("wikipedia")
+
+
+def prepare_wiki_collection():
+    """
+    Prepare the wikipedia collection
+
+    NOTE: Only needs to be run once to prepare the collection for the first time
+    """
+    ids, embeddings, documents, metadatas = zip(
+        *[(i, row['emb'], row['text'], row['title']) for i, row in enumerate(wikipedia_dataset)])
+
+    wikipedia_collection.add(
+        ids=ids,
+        embeddings=embeddings,
+        documents=documents,
+        metadatas=metadatas,
+    )
+
 
 if __name__ == "__main__":
     print(wikipedia_dataset.info)
+
+    print("Preparing wikipedia collection...")
+    prepare_wiki_collection()
+    print("Done!")

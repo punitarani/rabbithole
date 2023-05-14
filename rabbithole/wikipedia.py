b8cd00ee362b96d2e2ecdb43b00c5247f0b73a72
Punit Arani <punitsai36@gmail.com>
2023-05-13 17:52:24 -0700

    Update prepare_wikipedia_collection() to prepare in batches

--- rabbithole/wikipedia.py
+++ rabbithole/wikipedia.py
@@ -1,12 +1,19 @@
 """rabbithole.wikipedia module"""
 
+import chromadb.api
 from datasets import load_dataset
 from tqdm import tqdm
 
-from vecstore import client
+from rabbithole.vecstore import client
 
 wikipedia_dataset = load_dataset("Cohere/wikipedia-22-12-simple-embeddings", split="train", streaming=False)
-wikipedia_collection = client.create_collection("wikipedia")
+
+
+def get_wikipedia_collection() -> chromadb.api.Collection:
+    """
+    Get the wikipedia collection
+    """
+    return client.get_collection("wikipedia")
 
 
 def prepare_wikipedia_collection(batch_size: int = 10000):
@@ -15,23 +22,20 @@ def prepare_wikipedia_collection(batch_size: int = 10000):
 
     NOTE: Only needs to be run once to prepare the collection for the first time
     """
+    collection = client.create_collection("wikipedia")
+
     total_rows = len(wikipedia_dataset)
     for i in tqdm(range(0, total_rows, batch_size), desc='Processing batches'):
-        batch_data = wikipedia_dataset[i: i+batch_size]
-        ids, embeddings, documents, metadatas = zip(
-            *[(i, row['emb'], row['text'], row['title']) for i, row in enumerate(batch_data)])
+        batch_data = wikipedia_dataset[i: i + batch_size]
 
-        wikipedia_collection.add(
-            ids=ids,
-            embeddings=embeddings,
-            documents=documents,
-            metadatas=metadatas,
+        collection.add(
+            ids=[str(id) for id in batch_data["id"]],
+            embeddings=batch_data["emb"],
+            documents=batch_data["text"],
+            metadatas=[{"title": title} for title in batch_data["title"]],
         )
 
 
 if __name__ == "__main__":
     print(wikipedia_dataset.info)
-
-    print("Preparing wikipedia collection...")
     prepare_wikipedia_collection()
-    print("Done!")

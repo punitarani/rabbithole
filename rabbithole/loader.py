ef0849e132429926e6eb54b1dfdaf31223498fd8
Punit Arani <punitsai36@gmail.com>
2023-05-13 22:44:31 -0700

    Add save_to_temp_file()

--- rabbithole/loader.py
+++ rabbithole/loader.py
@@ -10,6 +10,18 @@ from rabbithole.mp3 import SUPPORTED_FILE_TYPES, convert_to_mp3
 from rabbithole.transcribe import transcribe
 
 
+def save_to_temp_file(file: UploadedFile) -> str:
+    """
+    Save an uploaded file to a temporary file and return the path.
+    :param file: File to save.
+    :return: Path to the temporary file.
+    """
+    temp_file = tempfile.NamedTemporaryFile(suffix=f".{file.name.split('.')[-1]}", delete=False)
+    temp_file.write(file.read())
+    temp_file.close()
+    return temp_file.name
+
+
 def load_file(file: UploadedFile) -> list[Document]:
     """
     Load a file and return a list of Document objects
@@ -19,23 +31,17 @@ def load_file(file: UploadedFile) -> list[Document]:
     """
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 
-    if file.type == "application/pdf":
-        # Save to temporary file
-        temp_file = tempfile.NamedTemporaryFile(delete=False)
-        temp_file.write(file.read())
-        temp_file.close()
-        # Load the file using PyMuPDF
-        return PyMuPDFLoader(file_path=temp_file.name).load_and_split(text_splitter=text_splitter)
+    # Handle PDF files
+    if file.name.endswith("pdf"):
+        temp_file = save_to_temp_file(file)
+        return PyMuPDFLoader(file_path=temp_file).load_and_split(text_splitter=text_splitter)
 
     # Handle Audio and Video files
     elif file.name.endswith(SUPPORTED_FILE_TYPES):
-        # Save to temporary file
-        temp_file = tempfile.NamedTemporaryFile(suffix=f".{file.name.split('.')[-1]}", delete=False)
-        temp_file.write(file.read())
-        temp_file.close()
+        temp_file = save_to_temp_file(file)
 
         # Convert to mp3 and transcribe
-        mp3_file = convert_to_mp3(temp_file.name)
+        mp3_file = convert_to_mp3(temp_file)
         transcription = transcribe(mp3_file)
 
         # Save transcription to temporary file
